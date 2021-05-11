01dbc8cb8c98404d723aec3c28d3edcb5ab80962
MUHSIN N <muhsinny333@gmail.com>
2021-05-11 16:17:38 +0530

    jquery added

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const navSlide = () => {
     const nav = document.querySelector('nav ul')
     nav.classList.toggle('nav-active')
 }
+$(window).on("scroll", function() {
+    if ($(this).scrollTop() > 100) {
+      $("nav").css("background","#252525");
+    }
+    else {
+      $("nav").css("background","transparent");
+   }
+});
